8be594bd66754e5968a130888e51bce250169c7e
Ellen Ohlsson <ellen.c.ohlsson@gmail.com>
2020-04-21 19:03:36 +0000

    Misc improvements to extend functionality

--- extend.py
+++ extend.py
@@ -8,21 +8,25 @@ def extend_transaction(transactions):
         {
         'description' : 'Bredband2',
         'original_description': 'BREDBAND2 AB',
-        'after_date' : date(2018, 1, 1),
-        'before_date': date(2020, 9, 1)
+        'start_date' : date(2018, 1, 1),
+        'end_date': date(2020, 9, 1)
         },
         {
         'description' : 'If Skadeförsäk',
         'original_description': 'IF SKADEFÖRSÄK',
-        'after_date' : date(2017, 12, 1),
-        'before_date': date(2020, 12, 1)
+        'start_date' : date(2017, 12, 1),
+        'end_date': date(2020, 12, 1)
         }
     ]
 
     for ext in extend_by:
 
         # Find all related transactions
-        related = [t for t in transactions if t.description == ext['description']]
+        related = [t
+            for t in transactions
+            if t.description == ext['description'] or
+               t.original_description == ext['original_description']
+        ]
 
         def _prev_and_nxt(some_iterable):
             prevs, items, nexts = tee(some_iterable, 3)
@@ -40,9 +44,15 @@ def extend_transaction(transactions):
         # Extend transaction to one month before the next related one
         for r_prev, r, r_next in _prev_and_nxt(related):
 
-            # TODO: make after_date and before_date optional
-            if r.date > ext['after_date'] and r.date < ext['before_date']:
-                if r_next:
-                    r.months_extend = _get_interval()
-                else:
-                    r.months_extend = r_prev.months_extend
+            if 'start_date' in ext:
+                if r.date < ext['start_date']:
+                    continue
+
+            if 'end_date' in ext:
+                if r.date > ext['end_date']:
+                    continue
+
+            if r_next:
+                r.months_extend = _get_interval()
+            else:
+                r.months_extend = r_prev.months_extend
